12923f3a828c5849271db4376df1b148d69c6e5c
Safi289 <ssahid151289@bscse.uiu.ac.bd.com>
2023-12-26 17:17:26 +0600

    first commit

---
+++ app.vue
@@ -0,0 +1,49 @@
+<style>
+@import url("~/public/assets/css/globals.css");
+@import url("~/public/assets/css/style.css");
+</style>
+<template>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="desktop">
+      <div class="div">
+        <div class="rectangle"></div>
+        <img class="mask-group" src="https://c.animaapp.com/x8PjqPJQ/img/mask-group.png" />
+        <div class="book-your-doctor">BOOK YOUR DOCTOR APPOINTMENT</div>
+        <div class="overlap-group">
+          <div class="text-wrapper">Select Your Service</div>
+          <img class="polygon" src="https://c.animaapp.com/x8PjqPJQ/img/polygon-1.svg" />
+        </div>
+        <div class="text-wrapper-2">About Doctor Appointment Tool</div>
+        <p class="lorem-ipsum-is">
+          Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting Industry. Lorem Ipsum Has Been The
+          Industry&#39;s Standard Dummy Text Ever Since The 1500s, When An Unknown Printer Took A Galley Of Type And
+          Scrambled It To Make A Type Specimen Book. It Has Survived Not Only Five Centuries, But Also The Leap Into
+          Electronic Typesetting, Remaining Essentially Unchanged. It Was Popularised In The 1960s With The Release Of
+          Letraset Sheets Containing Lorem Ipsum Passages, And More Recently With Desktop Publishing Software Like Aldus
+          Pagemaker Including Versions Of Lorem Ipsum.
+        </p>
+        <p class="p">
+          Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting Industry. Lorem Ipsum Has Been The
+          Industry&#39;s Standard Dummy Text Ever Since The 1500s.
+        </p>
+        <div class="overlap"><p class="PCR-test-for-COVID">Pcr Test For Covid (free)</p></div>
+        <div class="PCR-test-for-COVID-wrapper"><p class="PCR-test-for-COVID">Pcr Test For Covid (free)</p></div>
+        <div class="div-wrapper"><div class="text-wrapper-3">Doctor Avoidman</div></div>
+        <div class="overlap-2"><div class="text-wrapper-3">Doctor Avoidman</div></div>
+        <div class="flushot-high-dose-wrapper"><div class="flushot-high-dose">Flushot (high Dose)</div></div>
+        <div class="overlap-3"><div class="flushot-high-dose">Flushot (high Dose)</div></div>
+      </div>
+    </div>
+  </body>
+</html>
+
+</template>
+
